ef8dd8bfd859d5c498eec95eec6992928cbf909f
Noah <noikolomidze@gmail.com>
2023-07-07 18:28:31 +0400

    helpful maths added

---
+++ HelpfulMaths.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelpfulMaths {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] arr = s.split("\\+");
+        Arrays.sort(arr);
+        System.out.println(String.join("+", arr));
+
+
+    }
+}
